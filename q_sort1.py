4fe95aa2f4df7f077f861e07e477538f52d9fd91
Oscar Brink <oscbri-7@student.ltu.se>
2018-11-09 13:12:23 +0100

    incomplete stuff

--- q_sort1.py
+++ q_sort1.py
@@ -1,6 +1,14 @@
 from random import choice
 
+def quicksort(lst, f=None, pivot=None):
 
-def quicksort():
-    pass
+    if not (f is None):
+        pivot = f(lst)
+    elif pivot is None:
+        raise ValueError
+
+    lst1 = []
+    lst2 = []
+    for n in lst:
+        lst
 
