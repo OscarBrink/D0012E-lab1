bf3280eadf34e1351606c9906fd369e9efb5b66d
Oscar Brink <oscbri-7@student.ltu.se>
2018-11-09 13:44:30 +0100

    q_sort first impl done

--- q_sort1.py
+++ q_sort1.py
@@ -1,36 +1,41 @@
-from random import choice, randint, seed
-from time import time
-
-from graphics import Graphics
+from random import choice, randint
 
 def quicksort(lst, f=None, pivot=None):
 
+    if len(lst) <= 1:
+        return lst
+
     if not (f is None):
         pivot = f(lst)
+        lst.remove(pivot)
     elif pivot is None:
         raise ValueError
+
+#   print("pivot:", pivot)
 
     lst_lower = []
     lst_upper = []
     for n in lst:
+#       print(type(n), type(pivot))
         if n < pivot:
             lst_lower.append(n)
         else:
             lst_upper.append(n)
+#   print(lst_lower, lst_upper)
+#   print("gello:", len(lst_upper), len(lst_lower))
     
     if not (f is None):
-        pivot = f(lst)
-    elif pivot is None:
-        raise ValueError
+        return  quicksort(lst_lower, f=f) + [pivot] + quicksort(lst_upper, f=f)
+    else:
+        return  quicksort(lst_lower, pivot=lst_lower[len(lst_lower)//2]) + \
+                [pivot] + \
+                quicksort(lst_upper, pivot=lst_upper[len(lst_upper)//2])
 
+l = [randint(0, 100) for i in range (1000)]
+def f(lst):
+#   print(len(lst))
+    return lst[len(lst)//2]
 
-
-def generate_list(max_val, min_val, list_size):
-    seed(time())
-    output = []
-
-    for i in range(0, list_size):
-        output.append(randint(min_val, max_val))
-
-    return output
+print("in:", l)
+print("out: ", quicksort(l, f))
 
