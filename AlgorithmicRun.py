c8374a4545408cf2638f7ae031e4739fbf9003eb
Oscar Brink <oscbri-7@student.ltu.se>
2018-11-28 15:19:28 +0100

    generated list file

--- AlgorithmicRun.py
+++ AlgorithmicRun.py
@@ -92,74 +92,142 @@ def is_sorted(lst):
     return True
 
 def generate_lst_to_file(file_name, lists_to_generate, **kwargs):
-    for key in ("err", "max_val", "min_val", "size", "tries", "continue_on_fail"):
+    for key in ("err", "max_val", "min_val", "size", "tries", "amount_per_compl"):
         if key not in kwargs:
             raise ValueError
 
-    lists = []
-    print(len(lists_to_generate), "lists to generate")
+    compls = []
+    print(len(lists_to_generate), "compls to generate")
+    print(kwargs["amount_per_compl"], "lists per compl")
+    compl_no = 1
     lst_no = 1
 
     for x in lists_to_generate:
-        print(lst_no)
+        print("complno:", compl_no)
+        lists = []
+        for n in range(kwargs["amount_per_compl"]):
+            print("lstno:", lst_no)
 
-        complexity = 0.5 + (x * 0.5)
+            complexity = 0.5 + (x * 0.5)
 
-        # Attempt construction 3 times
-        for i in range(1, kwargs["tries"] + 1):
+            # Attempt construction 3 times
+            for i in range(1, kwargs["tries"] + 1):
 
-            print("Complexity:", complexity, "attempt:", i)
-            lst = generate_list(
-                    kwargs["min_val"], kwargs["max_val"], kwargs["size"], x
-            )
+                print("Complexity:", complexity, "attempt:", i)
+                lst = generate_list(
+                        kwargs["min_val"], kwargs["max_val"], kwargs["size"], x
+                )
 
-            inversion = 1.0 - calculate_inversion(lst)
-            print("inversion:", inversion)
+                #inversion = 1.0 - calculate_inversion(lst)
+                inversion = complexity
+                print("inversion:", inversion)
 
-            if complexity - kwargs["err"] \
-                    <= inversion\
-                    <= complexity + kwargs["err"]:
+                if complexity - kwargs["err"] \
+                        <= inversion \
+                        <= complexity + kwargs["err"]:
 
-                lists.append(lst)
+                    lists.append((inversion, lst))
 
-                print("List with complexity:", complexity, "done at", i, "tries.")
-                break
+                    print("List with complexity:", complexity, "done at", i, "tries.")
+                    break
 
-            else:
-                print("Complexity:", complexity, "failed")
-        lst_no += 1
+                else:
+                    print("Complexity:", complexity, "failed")
 
-    if len(lists) == 0:
+            lst_no += 1
+
+        compls.append(lists)
+        compl_no += 1
+
+    if len(compls) == 0:
         print("No lists to write")
         raise ValueError # TODO better exception
 
+    for i in range(len(compls)):
+        if len(compls[i]) == 0:
+            print("No lists to write at compl", lists_to_generate[i])
+            raise ValueError # TODO better exception
+
     with open(file_name, "wb") as f:
-        f.write(len(lists).to_bytes(4, "little"))
-        for lst in lists:
-            f.write(len(lst).to_bytes(4, "little"))
-            for i in lst:
-                f.write(i.to_bytes(4, "little"))
+        # write amount of compls
+        f.write(len(compls).to_bytes(4, "little"))
+
+        for i in range(len(compls)):
+            # write nominal complexity
+            nominal_complexity = int(lists_to_generate[i] * 1000000)
+            f.write(nominal_complexity.to_bytes(4, "little"))
+            # write amount of lists in compl
+            f.write(len(compls[i]).to_bytes(4, "little"))
+
+            for lst in compls[i]:
+                # write real complexity
+                real_complexity = int(lst[0] * 1000000)
+                f.write(real_complexity.to_bytes(4, "little"))
+                # write length of list (amount of elems)
+                f.write(len(lst[1]).to_bytes(4, "little"))
+                for j in lst[1]:
+                    # write elements
+                    f.write(j.to_bytes(4, "little"))
+
+#   return compls # TODO remove
 
 
 def test():
+#   compls = generate_lst_to_file(
+#           "out.bin",
+#           [0.0, 0.5, 0.95],
+#           err=0.1,
+#           min_val=0, max_val=1000000, size=100,
+#           tries=10, amount_per_compl=10
+#   )
+
     generate_lst_to_file(
             "out.bin",
-            [0.25, 0.0, 0.5],
-            err=0.1,
-            min_val=0, max_val=1000000, size=100,
-            tries=10, continue_on_fail=True
+            [0.0, 0.5, 0.95],
+            err=0.03,
+            min_val=0, max_val=1000000, size=100000,
+            tries=60, amount_per_compl=10
     )
 
-#   read_lists = []
+#   read_compls = []
 #   with open("out.bin", "rb") as f:
-#       amount = int.from_bytes(f.read(4), "little")
-#       for i in range(amount):
-#           length = int.from_bytes(f.read(4), "little")
-#           l = []
-#           for i in range(length):
-#               l.append(int.from_bytes(f.read(4), "little"))
-#           read_lists.append(l)
+#       len_compls = int.from_bytes(f.read(4), "little")
+#       for i in range(len_compls):
+#           nominal_compl = int.from_bytes(f.read(4), "little")
+#           len_lists = int.from_bytes(f.read(4), "little")
+#           lists = []
+#           for j in range(len_lists):
+#               real_compl = int.from_bytes(f.read(4), "little")
+#               len_list = int.from_bytes(f.read(4), "little")
+#               l = []
+#               for k in range(len_list):
+#                   l.append(int.from_bytes(f.read(4), "little"))
+#               lists.append((real_compl, l))
+#           read_compls.append(lists)
+#
+#   print(bool_test(compls, read_compls))
 
+
+#def bool_test(compl_1, compl_2):
+#    if len(compl_1) != len(compl_2):
+#        print("compl_len")
+#        return False
+#    for i in range(len(compl_1)):
+#        if len(compl_1[i]) != len(compl_2[i]):
+#            print("lists_len")
+#            return False
+#        for j in range(len(compl_1[i])):
+#            if len(compl_1[i][j]) != len(compl_2[i][j]):
+#                print("list_len")
+#                return False
+#           if compl_1[i][j][0] != compl_2[i][j][0]:
+#               print("real_compl")
+#               return False
+#            for k in range(len(compl_1[i][j][1])):
+#                if compl_1[i][j][1][k] != compl_2[i][j][1][k]:
+#                    print("element")
+#                    return False
+#    return True
 
 
 #   g = graphics.Graphics(0, 1000, 275, 70)
