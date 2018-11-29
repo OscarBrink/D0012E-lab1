67f127b9ee1d529d7cbb1b2b51af94804c359aef
josef <josutb-7@student.ltu.se>
2018-11-29 12:00:35 +0100

    Made quicsort inline

--- quicksort.py
+++ quicksort.py
@@ -1,10 +1,45 @@
 from random import choice
 import AlgorithmicRun
-import graphics 
+import graphics
 from math import ceil, sqrt
 
 
-def quicksort(lst, f=None):
+def quicksort(lst, f=None, start=0, end=None):
+
+    if end is None:
+        end = len(lst) - 1
+
+    if end - start == 2:
+        if lst[start] > lst[end - 1]:
+            lst[start], lst[end - 1] = lst[end - 1], lst[start]
+    if end - start <= 2:
+        return lst[start:end]
+
+    try:
+        pivot_index = lst.index(f(lst[start:end]))
+    except ValueError:
+        print("Pivot value is not an element of lst")
+        return
+
+    lst[pivot_index], lst[start] = lst[start], lst[pivot_index]
+    pivot_index = start
+
+    for i in range(start + 1, end + 1):
+        if lst[i] <= lst[pivot_index]:
+            lst.insert(pivot_index, lst.pop(i))
+            pivot_index += 1
+
+    return quicksort(lst, f, start, pivot_index) + quicksort(lst, f, pivot_index, end)
+
+
+def temp2(lst):
+    return lst[len(lst) // 2]
+
+
+# print(quicksort([2, 45, 2, 1, 5, 3, 7, 12, 4], temp2))
+
+
+def quicksort2(lst, f=None):
     # Base Case
     if len(lst) <= 1:
         return lst
@@ -30,10 +65,13 @@ def quicksort(lst, f=None):
 
 
 def middle_pivot(lst): return lst[len(lst) // 2]
+
+
 def random_pivot(lst): return choice(lst)
 
+
 def n_median_pivot(lst, amount):
-#   print("len:", len(lst))
+    #   print("len:", len(lst))
     step = len(lst) // (amount - 1)
     index = 0
     elems = []
@@ -53,22 +91,23 @@ def n_median_pivot(lst, amount):
         # Copy lst into elems if smaller than amount
         elems = lst[:]
 
-#   print("elems:", len(elems))
+    #   print("elems:", len(elems))
     # for now inefficient sort
-#   print(elems)
+    #   print(elems)
 
     # Insertion sorting the selected elements
     for i in range(len(elems)):
         j = i
-        while j > 0 and elems[j] < elems[j-1]:
-            temp = elems[j-1]
-            elems[j-1] = elems[j]
+        while j > 0 and elems[j] < elems[j - 1]:
+            temp = elems[j - 1]
+            elems[j - 1] = elems[j]
             elems[j] = temp
             j -= 1
 
-#   print(elems)
-#   print(elems[ceil(len(elems)/2) - 1])
-    return elems[ceil(len(elems)/2) - 1]
+    #   print(elems)
+    #   print(elems[ceil(len(elems)/2) - 1])
+    return elems[ceil(len(elems) / 2) - 1]
+
 
 med_3 = lambda lst: n_median_pivot(lst, 3)
 med_5 = lambda lst: n_median_pivot(lst, 5)
@@ -77,6 +116,8 @@ med_7 = lambda lst: n_median_pivot(lst, 7)
 
 def to_run(l):
     return quicksort(l, med_7)
+
+
 #   return quicksort(l, random_pivot)
 def shite():
     AlgorithmicRun.max_val = 1000000
